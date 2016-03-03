b923bb1b38ca8f252c945d17c61e318768a9ab9b
Derek Roberts <derek.roberts@gmail.com>
2016-03-03 00:34:16 -0800

    Recuded index, passing more to xml_builder

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Requires for general utils and mongo interface
 var ut = require( './lib/util.js' );
 var mi = require( './lib/mongo_interface.js' );
+var xb = require( './lib/xml_builder.js' );
 
 // Receive processed query input list from query_list.js
 var query_list = require( './lib/query_list.js' );
@@ -10,22 +11,9 @@ var titles     = query_list.titles();
 var doctor_list = require( './lib/doctor_list.js' );
 var doctors     = doctor_list.doctors();
 
-// Fields to return
-var filter = JSON.parse( '{ "title": 1, "executions": 1 }' );
-
 // XML array, to process
-var toExport = [];
+var toExport = doctors;
 
-// Collect query resuts
-var l = titles.length;
-for( var i = 0; i < l; i++ ){
-	var title = JSON.parse( '{ "title" : "'+ titles[ i ] +'" }' );
-	mi.find( title, filter, function( error, docs ){
-		if( error ){
-			console.log( error );
-			process.exit( 1 );
-		}
-		toExport.push( docs );
-		console.log( toExport );
-	});
-}
+// Replace PDC queries to pre-format as HDC/AMCARE queries
+
+// ToDo: Export toExport
