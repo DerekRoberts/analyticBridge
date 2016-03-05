54907580c2792cad83153bc9b74bf15e457d0267
Derek Roberts <derek.roberts@gmail.com>
2016-03-05 12:17:58 -0800

    +ScoreCard.json template, loads in index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// ScoreCard blank
+var scorecard = require( './config/scorecard.json' );
+
 // XML output
 var parser = require( 'xml2json' );
 
 // Raw doctor data for XML headers
 var headers = require( './lib/headers.js' ).raw;
+//console.log( headers );
 
 // Obtain queries, specified in ./lib/queries.json
 var executions = require( './lib/executions.js' );
 executions.executions( function( error, results ){
   if( error ){ throw new Error( error )}
 
-  console.log( results );
+  //console.log( results );
   return results;
 });
