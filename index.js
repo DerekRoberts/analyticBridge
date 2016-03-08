4f71c55b7619fbc88cd51d7013b85324acb51162
Derek Roberts <derek.roberts@gmail.com>
2016-03-08 13:09:49 -0800

    +toXml() function, converts JSON to XML

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ var parser = require( 'xml2json' );
 
 // Raw doctor data for XML headers
 var headers = require( './lib/headers.js' ).raw;
-//console.log( headers );
+var target = headers.cpsid;
 
 // Obtain queries, specified in ./lib/queries.json
 var executions = require( './lib/executions.js' );
 executions.executions( function( error, results ){
   if( error ){ throw new Error( error )}
 
-  //console.log( results );
   return results;
 });
+
+// Pretty print a scorecard
+function toXml( completed_scorecard ){
+  var pd = require( 'pretty-data' ).pd;
+  return pd.xml( parser.toXml( completed_scorecard ));
+}
+
+var xml = toXml( scorecard );
+console.log( xml );
