0a7787539d0e2c508e998f606020f77d8c465951
Derek Roberts <derek.roberts@gmail.com>
2016-03-10 00:09:36 -0800

    index.js - added build for doctor/query object

--- index.js
+++ index.js
@@ -2,14 +2,49 @@
 var scorecard = require( './config/scorecard.json' );
 
 // Raw doctor data for XML headers
-var headers = require( './lib/headers.js' ).raw;
+var headers = require( './lib/headers.js' );
+
+// Store processed doctor and query data
+var doc_data = [];
+
+function doc_builder( results ){
+  var doctors = headers.doctors();
+
+  doctors.forEach( function( doc ){
+
+    // Create map for doc name, header, PatientCounts and ReportingCategories
+    doc_data[ doc ] = [];
+    doc_data[ doc ][ 'header' ] = headers.raw[ doc ];
+    doc_data[ doc ][ 'PatientCounts' ] = [];
+    doc_data[ doc ][ 'ReportingCategories' ] = [];
+
+    // If there are PatientCounts, add them
+    if( results[ doc ] && results[ doc ][ 'PatientCounts' ]){
+      doc_data[ doc ][ 'PatientCounts' ]= results[ doc ][ 'PatientCounts' ];
+      delete results[ doc ][ 'PatientCounts' ];
+    }
+
+    if( typeof results[ doc ] == 'object' && Object.keys(results[doc])){
+      Object.keys( results[ doc ]).forEach( function( query ){
+        if( results[ doc ][ query ][ 0 ].category === 'ReportingCategories' ){
+          doc_data[ doc ][ 'ReportingCategories' ][ query ] = results[ doc ][ query ];
+        }
+      });
+    }
+  });
+
+  // TODO: Make this less of a mess
+  // TODO: Make a clean return function
+  return doc_data;
+}
 
 // Obtain queries, specified in ./lib/queries.json
 var executions = require( './lib/executions.js' );
-executions.executions( function( error, results ){
+var blah = executions.executions( function( error, results ){
   if( error ){ throw new Error( error )}
 
-  return results;
+  console.log( doc_builder( results ));
+  return doc_builder( results );
 });
 
 // Pretty print a scorecard
